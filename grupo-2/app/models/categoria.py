455ca5fbaea20759faeceb0206e03c91d07fa7d5
Antony <antony.programmer@gmail.com>
2026-06-19 17:35:11 -0400

    feat: modelo de categoria

---
+++ grupo-2/app/models/categoria.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from app.extensions import db
+
+
+class Categoria(db.Model):
+    __tablename__ = "categoria"
+
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(100), nullable=False, unique=True)
+    descripcion = Column(String(255), nullable=True)
+
+    def __repr__(self):
+        return self.nombre

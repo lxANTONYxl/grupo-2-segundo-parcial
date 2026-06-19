024be1744993bebbdd2f630f22de042d7ed9b0d2
Antony <antony.programmer@gmail.com>
2026-06-19 17:38:42 -0400

    feat: archivo init con los modelos

---
+++ grupo-2/app/models/__init__.py
@@ -0,0 +1,6 @@
+from .categoria import Categoria
+from .marca import Marca
+from .laptop import Laptop
+from .cliente import Cliente
+from .pedido import Pedido
+from .detalle_pedido import DetallePedido

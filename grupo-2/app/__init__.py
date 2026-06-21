a59590fd6217adbaaef36b516e7c99e6f04f612f
Antony <antony.programmer@gmail.com>
2026-06-21 05:05:31 -0400

    feat: se importaron los modelos al init

--- grupo-2/app/__init__.py
+++ grupo-2/app/__init__.py
@@ -6,9 +6,18 @@ from .extensions import appbuilder, db
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_object("config")
+    
+    db.init_app(app)
+    
     with app.app_context():
-        appbuilder.init_app(app)
+        appbuilder.init_app(app, db.session)
+        
+        from app.models.categoria import Categoria
+        from app.models.marca import Marca
+        from app.models.laptop import Laptop
+        from app.models.cliente import Cliente
+        from app.models.pedido import Pedido
+        from app.models.detalle_pedido import DetallePedido
+        
         db.create_all()
-        # Registering the views and APIs
-        ...
     return app
